a8b7df67cda54f8fb9287aebfc7370cb37304a5d
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-02-24 16:30:26 +0530

    searching another dom implemented

--- searchAnotherDOM.js
+++ searchAnotherDOM.js
@@ -1,14 +1,18 @@
-const folderPath = "./otherFiles";
+const folderPath = "./pages";
 const parser = new DOMParser();
 
 const fetchAnotherDomBtn = document.getElementById("fetchAnotherDomBtn");
+// const searchInput = document.getElementById("search-input");
+
+fetchAnotherDomBtn.addEventListener("click", () => {
+  fetchAnotherDOM();
+});
 
 function fetchAnotherDOM() {
   fetch(folderPath)
     .then((response) => response.text())
     .then((data) => {
       const htmlFiles = data.match(/href="([^"]*\.html)"/g);
-      // console.log(htmlFiles);
       if (htmlFiles) {
         htmlFiles.forEach((file) => {
           const fileName = file.match(/href="([^"]*\.html)"/)[1];
@@ -16,10 +20,10 @@ function fetchAnotherDOM() {
             .then((response) => response.text())
             .then((data) => {
               const dom = parser.parseFromString(data, "text/html");
-              // do something with the DOM of the file
               dom.body.childNodes.forEach((node) => {
                 if (!node.length && node.nodeName !== "SCRIPT") {
-                  console.log(node);
+                  //   console.log(node);
+                  searchingAnotherDOM(node);
                 }
               });
             })
@@ -30,4 +34,28 @@ function fetchAnotherDOM() {
       }
     })
     .catch((err) => console.error(err));
+}
+
+// Define search function
+function searchingAnotherDOM(DOM) {
+  const searchTerm = searchInput.value;
+  const highlights = DOM.querySelectorAll(".highlight");
+  for (let i = 0; i < highlights.length; i++) {
+    const textNode = document.createTextNode(highlights[i].textContent);
+    highlights[i].parentNode.replaceChild(textNode, highlights[i]);
+  }
+
+  const regex = new RegExp(searchTerm, "gi");
+  const matches = DOM.innerHTML.match(regex);
+
+  if (matches) {
+    DOM.childNodes.forEach((node) => {
+      if (!node.length) {
+        node.innerHTML = node.innerHTML.replace(
+          regex,
+          '<span class="highlight">$&</span>'
+        );
+      }
+    });
+  }
 }
